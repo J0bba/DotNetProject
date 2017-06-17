7f0191e2e10ae2bd66e7a9d9e10a2ee16730dbbd
Thibault Virsolvy <Thibault Virsolvy>
2017-06-17 21:17:58 +0200

    added message handling in back

---
+++ MeditateBook/BusinessManagement/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeditateBook.BusinessManagement
+{
+    public class Message
+    {
+        public static bool CreateMessage(DBO.Message message)
+        {
+            return DataAccess.Message.CreateMessage(message);
+        }
+
+        public static bool DeleteMessage(long id)
+        {
+            return DataAccess.Message.DeleteMessage(id);
+        }
+
+        public static bool UpdateMessage(DBO.Message message)
+        {
+            return DataAccess.Message.UpdateMessage(message);
+        }
+
+        public static List<DBO.Message> GetListMessageByUser(long user_id)
+        {
+            return DataAccess.Message.GetListMessageByUser(user_id);
+        }
+    }
+}
---
+++ MeditateBook/DataAccess/Message.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeditateBook.DataAccess
+{
+    public class Message
+    {
+        public static bool CreateMessage(DBO.Message message)
+        {
+            try
+            {
+                using (MeditateBookEntities bdd = new MeditateBookEntities())
+                {
+                    T_Message newMessage = new T_Message()
+                    {
+                        content = message.Content,
+                        date = message.Date,
+                        id_receiver = message.IdReceiver,
+                        id_sender = message.IdSender
+                    };
+
+                    bdd.T_Message.Add(newMessage);
+                    bdd.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                return false;
+            }
+        }
+
+        public static bool DeleteMessage(long id)
+        {
+            try
+            {
+                using (MeditateBookEntities bdd = new MeditateBookEntities())
+                {
+                    bdd.T_Message.Remove(bdd.T_Message.Where(x => x.id == id).FirstOrDefault());
+                    bdd.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                return false;
+            }
+        }
+
+        public static bool UpdateMessage(DBO.Message message)
+        {
+            try
+            {
+                using (MeditateBookEntities bdd = new MeditateBookEntities())
+                {
+                    T_Message oldMessage = bdd.T_Message.Where(x => x.id == message.Id).FirstOrDefault();
+                    oldMessage.content = message.Content;
+                    oldMessage.date = message.Date;
+                    oldMessage.id_receiver = message.IdReceiver;
+                    oldMessage.id_sender = message.IdSender;
+                    bdd.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                return false;
+            }
+        }
+
+        public static List<DBO.Message> GetListMessageByUser(long user_id)
+        {
+            List<DBO.Message> result = new List<DBO.Message>();
+            try
+            {
+                using (MeditateBookEntities bdd = new MeditateBookEntities())
+                {
+                    List<T_Message> list = bdd.T_Message.Where(x => x.id_sender == user_id || x.id_receiver == user_id).ToList();
+                    foreach (T_Message message in list)
+                    {
+                        DBO.Message newMessage = new DBO.Message()
+                        {
+                            Content = message.content,
+                            Date = message.date,
+                            Id = message.id,
+                            IdReceiver = message.id_receiver,
+                            IdSender = message.id_sender
+                        };
+                        result.Add(newMessage);
+                    }
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                return result;
+            }
+        }
+    }
+}
