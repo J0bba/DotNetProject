7607ae04d556429aee8131f377c4d983f562791b
Thibault Virsolvy <Thibault Virsolvy>
2017-06-17 15:58:56 +0200

    added localization files

---
+++ MeditateBook/DataAccess/Language.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MeditateBook.DataAccess
+{
+    public class Language
+    {
+        public static bool CreateLanguage(DBO.Language language)
+        {
+            try
+            {
+                using (MeditateBookEntities bdd = new MeditateBookEntities())
+                {
+                    T_Language newLanguage = new T_Language()
+                    {
+                        name = language.Name,
+                        shortname = language.ShortName
+                    };
+                    bdd.T_Language.Add(newLanguage);
+                    bdd.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                return false;
+            }
+        }
+
+        public static bool DeleteLanguage(long id)
+        {
+            try
+            {
+                using (MeditateBookEntities bdd = new MeditateBookEntities())
+                {
+                    bdd.T_Language.Remove(bdd.T_Language.Where(x => x.id == id).FirstOrDefault());
+                    bdd.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                return false;
+            }
+        }
+
+        public static UpdateLanguage(DBO.Language language)
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
