10480804b6fb255df77e6e704b7bb6e053cd8e3a
Thibault Virsolvy <Thibault Virsolvy>
2017-06-21 15:16:45 +0200

    started from old t^p with good connection

--- MeditateBook/Properties/AssemblyInfo.cs
+++ MeditateBook/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l’ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("f00658b9-c2ae-4a7e-ad5d-f016a8837f40")]
+[assembly: Guid("1865c029-e253-4156-bad2-df7fcbe821d2")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
---
+++ MeditateBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeditateBook.Startup))]
+namespace MeditateBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
