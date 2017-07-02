eab5cb75c4510a031a796244bca28db528423707
DESKTOP-F049I5T\Maximilien <maximiliengomes@hotmail.fr>
2017-07-02 09:46:22 +0200

    Add missing files for adding articles

---
+++ MeditateBook/Controllers/EditArticleController.cs
@@ -0,0 +1,33 @@
+﻿using MeditateBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeditateBook.Controllers
+{
+    public class EditArticleController : _BaseController
+    {
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [HttpPost, ValidateInput(false)]
+        public ActionResult Submit(EditArticleModel model)
+        {
+            DBO.Article article = new DBO.Article { Title = model.Title, Content = model.Content, Validated = false, CreatedDate = DateTime.Now };
+            var idCreator = HttpContext.Session["UserID"];
+            if (idCreator != null)
+                article.IdCreator = (long)idCreator;
+            BusinessManagement.Article.CreateArticle(article);
+            return View();
+        }
+
+
+        public ActionResult EditArticle()
+        {
+            return View();
+        }
+    }
+
+    
+}
---
+++ MeditateBook/Models/EditArticleModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MeditateBook.Models
+{
+    public class EditArticleModels
+    {
+    }
+
+    public class EditArticleModel
+    {
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+    }
+}
