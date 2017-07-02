b636db11247ca838c3b62742915965131cf6888a
Yassine Ammar <ammar.yassine@yahoo.fr>
2017-07-02 14:17:29 +0200

    Added bulma css Added Search controller and logic, just need functions to actually search articles

---
+++ MeditateBook/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using MeditateBook.Models;
+using System.Web.Mvc;
+
+namespace MeditateBook.Controllers
+{
+    public class SearchController : _BaseController
+    {
+        // GET: Search
+        public ActionResult Index(string searchString)
+        {
+            SearchViewModel model = new SearchViewModel();
+            model.searchedText = searchString;
+            return View(model);
+        }
+    }
+}
---
+++ MeditateBook/Models/SearchViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeditateBook.Models
+{
+
+    public class SearchViewModel
+    {
+        public string searchedText { get; set; }
+        public List<DBO.Article> Results { get; set; }
+    }
+}
