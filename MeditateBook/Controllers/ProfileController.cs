8635a2e872426c75585e5446542cc2e5b5b28fe6
DESKTOP-F049I5T\Maximilien <maximiliengomes@hotmail.fr>
2017-07-03 19:12:09 +0200

    Missing controller

---
+++ MeditateBook/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using MeditateBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeditateBook.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index(long id)
+        {
+            ProfileViewModel model = new ProfileViewModel();
+            model.User = BusinessManagement.User.GetUserById(id);
+            model.idCurrentUser = (long)HttpContext.Session["UserID"];
+            return View(model);
+        }
+    }
+}
