0f0eb213a751a5717848b41392d047ee368c1e00
DESKTOP-F049I5T\Maximilien <maximiliengomes@hotmail.fr>
2017-07-04 05:10:27 +0200

    download file without extension

--- MeditateBook/Controllers/ArticlesController.cs
+++ MeditateBook/Controllers/ArticlesController.cs
@@ -211,9 +211,11 @@ namespace MeditateBook.Controllers
             for (int i = 0; i < list.Count; i++)
             {
                 if (list[i].Id == id)
-                    return File(list[i].FilePath, Server.UrlEncode(list[i].FilePath));
+                    return new FilePathResult(list[i].FilePath, System.Net.Mime.MediaTypeNames.Application.Octet);
+               // return File(list[i].FilePath, Server.UrlEncode(list[i].FilePath));
             }
             return RedirectToAction("Index", "Home");
+            
         }
     }
 }
